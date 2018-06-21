7f58a4dba0c6b2011f4c3a458699187e3274a8c1
jiyoungO <ji9310@naver.com>
2018-06-21 19:34:10 +0900

    add lapism search View

--- app/src/main/java/jiyoung/example/kotlin/com/kotlinsamples/MainActivity.kt
+++ app/src/main/java/jiyoung/example/kotlin/com/kotlinsamples/MainActivity.kt
@@ -1,9 +1,8 @@
 package jiyoung.example.kotlin.com.kotlinsamples
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.Toast
 import jiyoung.example.kotlin.com.kotlinsamples.network.NetworkCheckReceiver
 import jiyoung.example.kotlin.com.kotlinsamples.network.NetworkModel
 import jiyoung.example.kotlin.com.kotlinsamples.network.NetworkModel.setStatus
@@ -42,17 +41,10 @@ class MainActivity : AppCompatActivity() {
 
         networkModel.execute()
 
-        button1.setOnClickListener(View.OnClickListener { view -> nullTest(this.setViewText(null)) })
+        BtnLapism.setOnClickListener { view -> startActivity(Intent(this@MainActivity, ActLapism::class.java)) }
 
     }
 
-    private fun setViewText(text: String?) : String?{
-        return text
-    }
-
-    private fun nullTest(message: String?) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
 
 
 }
